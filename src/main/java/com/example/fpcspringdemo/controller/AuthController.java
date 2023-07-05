4d5de7e06e2710f244187109503fea33f3e5aa9f
EliteFan <fan552426811@outlook.com>
2023-07-06 07:16:16 +0800

    all

--- src/main/java/com/example/fpcspringdemo/controller/AuthController.java
+++ src/main/java/com/example/fpcspringdemo/controller/AuthController.java
@@ -5,6 +5,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,35 +31,41 @@ public class AuthController {
     public Object auth() {
         return new Result("哈哈get",true);
     }
-    @PostMapping("/auth")
-    @ResponseBody
-    public Object authP() {
-        return new Result("post",true);
-    }
     @PostMapping("/auth/login")
     public void login(@RequestBody Map<String, Object> usernameAndPassword) {
         String username = (String) usernameAndPassword.get("username");
         String password = (String) usernameAndPassword.get("password");
-//        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+        try{
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+        }catch(UsernameNotFoundException e){
+
+        }
 //        UsernamePasswordAuthenticationToken token =
 //                new UsernamePasswordAuthenticationToken(userDetails, password, userDetails.getAuthorities());
     }
 
     private static class Result {
         private String status;
-        private Boolean login;
+        private String msg;
+        private boolean login;
+
+        public Result(String status, String msg, boolean login) {
+            this.status = status;
+            this.msg = msg;
+            this.login = login;
+        }
 
         public String getStatus() {
             return status;
         }
 
-        public Boolean getLogin() {
-            return login;
+        public String getMsg() {
+            return msg;
         }
 
-        public Result(String status, Boolean login) {
-            this.status = status;
-            this.login = login;
+        public boolean isLogin() {
+            return login;
         }
     }
 }
