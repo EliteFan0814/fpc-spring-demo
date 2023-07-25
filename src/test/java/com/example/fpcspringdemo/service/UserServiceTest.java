f57517a09de2bce097e96c0062f3e1d5df8cf8a7
EliteFan <fan552426811@outlook.com>
2023-07-25 22:55:12 +0800

    all

--- src/test/java/com/example/fpcspringdemo/service/UserServiceTest.java
+++ src/test/java/com/example/fpcspringdemo/service/UserServiceTest.java
@@ -1,15 +1,20 @@
 package com.example.fpcspringdemo.service;
 
+import com.example.fpcspringdemo.entity.User;
 import com.example.fpcspringdemo.mapper.UserMapper;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
     @Mock
@@ -17,11 +22,34 @@ class UserServiceTest {
     @Mock
     UserMapper userMapper;
     @InjectMocks
-    UserService userService=new UserService(userMapper,mockEncoder);
+    UserService userService;
+
     @Test
-    public void testSave(){
+    public void testSave() {
         Mockito.when(mockEncoder.encode("test")).thenReturn("encodedTest");
-        userService.save("test","test");
-        Mockito.verify(userService).save("test","encodedTest");
+        userService.save("test", "test");
+        Mockito.verify(userMapper).saveUser("test", "encodedTest");
+    }
+
+    @Test
+    public void testGetUserByUserName() {
+        userService.getUserByUserName("fpc");
+        Mockito.verify(userMapper).findUserByUserName("fpc");
+    }
+
+    @Test
+    public void testLoadUserByUsernameThrowException() {
+        Mockito.when(userMapper.findUserByUserName("fpc")).thenReturn(null);
+        Assertions.assertThrows(UsernameNotFoundException.class, () -> {
+            userService.loadUserByUsername("fpc");
+        });
+    }
+
+    @Test
+    public void testLoadUserByUsername() {
+        Mockito.when(userMapper.findUserByUserName("fpc")).thenReturn(new User(1, "fpc", "encodedPassword", null));
+        UserDetails userDetail = userService.loadUserByUsername("fpc");
+        Assertions.assertEquals("fpc", userDetail.getUsername());
+        Assertions.assertEquals("encodedPassword", userDetail.getPassword());
     }
 }
