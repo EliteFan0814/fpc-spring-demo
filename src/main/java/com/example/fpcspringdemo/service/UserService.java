0789f1829408022aada39a80231d1803fae2f6bf
fanpeichao <fan552426811@outlook.com>
2023-08-02 17:51:09 +0800

    all

--- src/main/java/com/example/fpcspringdemo/service/UserService.java
+++ src/main/java/com/example/fpcspringdemo/service/UserService.java
@@ -15,33 +15,31 @@ import java.util.Collections;
 public class UserService implements UserDetailsService {
     private UserMapper userMapper;
     private BCryptPasswordEncoder bCryptPasswordEncoder;
-//    private Map<String, User> users = new ConcurrentHashMap<>();
 
     @Inject
     public UserService(UserMapper userMapper, BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
         this.userMapper = userMapper;
-//        this.save("fpc", "fpc");
     }
 
-    //    保存用户名和加密后的密码
+    // 保存用户名和加密后的密码
     public void save(String username, String password) {
         userMapper.saveUser(username, bCryptPasswordEncoder.encode(password));
-//        users.put(username, new User(1, username, bCryptPasswordEncoder.encode(password), ""));
     }
 
+    // 返回自己创建的user
     public User getUserByUserName(String username) {
         return userMapper.findUserByUserName(username);
-//        return users.get(username);
     }
 
+    // 返回security创造的user
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = getUserByUserName(username);
         if (user == null) {
             throw new UsernameNotFoundException("用户不存在");
         }
-        return new org.springframework.security.core.userdetails
-                .User(username, user.getEncryptedPassword(), Collections.emptyList());
+        return new org.springframework.security.core.userdetails.User(username, user.getEncryptedPassword(),
+                Collections.emptyList());
     }
 }
